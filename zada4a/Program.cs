e4e377103a08d5c107c9b460fa15bd050c96332d
Alexei Papush <aleksei.leongardt@yandex.ru>
2022-09-20 15:47:00 +0300

    fills the array

---
+++ zada4a/Program.cs
@@ -0,0 +1,12 @@
+﻿// Решаем поставленную задачу
+
+Console.Write("Введите размер массива:");
+int A = Convert.ToInt32(Console.ReadLine());
+string[] mas1 = new string[A];
+string[] mas2 = new string[mas1.Length];
+
+for (int i = 0; i < mas1.Length; i++)
+{
+    Console.Write("Введите элемент массива:");
+    mas1[i]=Convert.ToString(Console.ReadLine());
+}
